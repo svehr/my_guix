0503d15324b3d1c78c56ad11457b73dc87d79f41
svehr <svehr@svehr.dev>
2025-09-29 20:14:17 +0200

    add github host in ssh config for user svehr
    Add a remote in git like this to select the right identity file
    ```sh
    git remote add github git@svehr.github:svehr/my_guix.git
    ```

--- config/home/home.scm
+++ config/home/home.scm
@@ -158,7 +158,13 @@
 
     (service home-openssh-service-type
              (home-openssh-configuration
-              (add-keys-to-agent "yes")))
+              (add-keys-to-agent "yes")
+              (hosts
+               (list (openssh-host
+                      (name "svehr.github")
+                      (host-name "github.com")
+                      (user "git")
+                      (identity-file (string-append (getenv "HOME") "/.ssh/slim@sliook_->_svehr@github.com")))))))
 
     (service home-dbus-service-type)
     (service home-pipewire-service-type
